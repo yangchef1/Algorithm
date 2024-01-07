f9cc9c258dbf77267199393d20222845e22ecaf4
HeeChul Yang <113877384+yangchef1@users.noreply.github.com>
2024-01-07 18:05:28 +0900

    [level 1] Title: 최소직사각형, Time: 5.70 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/README.md
+++ 프로그래머스/1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=python3) 
 
 ### 성능 요약
 
-메모리: 11.3 MB, 시간: 5.34 ms
+메모리: 11.3 MB, 시간: 5.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 0일 17:49:16
+2024년 1월 0일 18:5:25
 
 ### 문제 설명
 
--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,33 +1,8 @@
-def compare(size1, size2):
-    result = []
-    if size1[0] > size2[0]:
-        result.append(size1[0])
-    else:
-        result.append(size2[0])
-    
-    if size1[1] > size2[1]:
-        result.append(size1[1])
-    else:
-        result.append(size2[1])
-        
-    return result
-    
 def solution(sizes):
-    answer = 0
-    wallet_size = [0, 0]
+    max_width, max_height = 0, 0
+
     for size in sizes:
-        if ((wallet_size[0] - size[0]) * (wallet_size[1] - size[1])) > 0:
-            wallet_size[0] = max(wallet_size[0], size[0])
-            wallet_size[1] = max(wallet_size[1], size[1])
-        else:
-            not_rotated = compare(wallet_size, size)
-            size.reverse()
-            rotated = compare(wallet_size, size)
-            
-            if rotated[0] * rotated[1] > not_rotated[0] * not_rotated[1]:
-                wallet_size = not_rotated
-            else:
-                wallet_size = rotated
-        
-    answer = wallet_size[0] * wallet_size[1]
-    return answer
+        max_width = max(max_width, max(size))
+        max_height = max(max_height, min(size))
+
+    return max_width * max_height
