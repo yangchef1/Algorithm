31cfd6f4f32c0301b768706118cc77059a12dfe7
HeeChul Yang <113877384+yangchef1@users.noreply.github.com>
2024-05-15 15:26:49 +0900

    [Bronze III] Title: TGN, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.py
@@ -0,0 +1,11 @@
+t = int(input())
+
+for _ in range(t):
+    r, e, c = map(int, input().split())
+    
+    if r < e - c:
+        print("advertise")
+    elif r == e - c:
+        print("does not matter")
+    else:
+        print("do not advertise")
