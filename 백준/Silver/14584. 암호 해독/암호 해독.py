c7d45716c64a739e1d3058c510fea3923079bf26
HeeChul Yang <113877384+yangchef1@users.noreply.github.com>
2025-02-18 16:12:14 +0900

    [Silver IV] Title: 암호 해독, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14584. 암호 해독/암호 해독.py
+++ 백준/Silver/14584. 암호 해독/암호 해독.py
@@ -4,9 +4,7 @@ words = [input() for _ in range(n)]
 
 for w in words:
   for i in range(26):
-    decrypt = ""
-    for c in w:
-      decrypt += chr(ord(c) + i if ord(c) + i <= ord("z") else ord(c) + i - 26)
+    decrypt = "".join(chr(ord(c) + i if ord(c) + i <= ord("z") else ord(c) + i - 26) for c in w)
     if decrypt in encrypt:
       print("".join(chr(ord(c) - i if ord(c) - i >= ord("a") else ord(c) - i + 26) for c in encrypt))
       exit(0)
