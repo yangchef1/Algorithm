5ed7ef2903173aba5bcca89805904c9d8d7074da
HeeChul Yang <113877384+yangchef1@users.noreply.github.com>
2025-10-06 01:39:16 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 58.82 ms, Memory: 16 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,19 +1,20 @@
+from collections import deque
+
 def solution(gems):
     l = len(gems)
     answer = [1, l]
     count = len(set(gems))
     
-    g_count = {}
-        
+    type = deque()
+    
     left = 0
     for right in range(l):
-        g_count[gems[right]] = g_count.get(gems[right], 0) + 1
-        while len(g_count) == count:
+        type.append(gems[right])
+        while len(set(type)) == count:
+            left += 1
+            type.popleft()
             if answer[1] - answer[0] > right - left:
                 answer = [left + 1, right + 1]
-            g_count[gems[left]] -= 1
-            if g_count.get(gems[left], 0) == 0:
-                del g_count[gems[left]]
-            left += 1
+                break
         
     return answer
