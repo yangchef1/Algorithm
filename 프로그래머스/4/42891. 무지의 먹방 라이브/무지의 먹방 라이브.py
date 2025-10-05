115076b927f2a07fa518c5b5463cb6e0c66a7448
HeeChul Yang <113877384+yangchef1@users.noreply.github.com>
2025-10-06 00:01:27 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 305.35 ms, Memory: 38.4 MB -BaekjoonHub

---
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
@@ -0,0 +1,22 @@
+import heapq
+
+def solution(food_times, k):
+    if sum(food_times) <= k:
+        return -1
+    
+    l = len(food_times)
+    
+    foods = []
+    for i in range(l):
+        heapq.heappush(foods, (food_times[i], i + 1))
+    
+    cur_time = 0
+    cycle = 0
+    
+    while foods and cur_time + (len(foods) * (foods[0][0] - cycle)) <= k:
+        t, i = heapq.heappop(foods)
+        cur_time += ((len(foods) + 1) * (t - cycle))
+        cycle = t
+    
+    foods.sort(key=lambda x : x[1])
+    return foods[(k - cur_time) % len(foods)][1]
